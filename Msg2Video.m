5f9da53d8d12565abdd73a5f3302098cebd91a79
Amelia Hines <122924959+ahines48@users.noreply.github.com>
2024-09-30 19:56:43 -0500

    Update Msg2Video.m

--- Msg2Video.m
+++ Msg2Video.m
@@ -1,23 +1,21 @@
 function Msg2Video() 
     % Read in message file and convert to binary
     byteFile = fopen('inputOpticalMsg.bin');
-    message_bytes = fread(byteFile);
-    binaryMessage = dec2bin(message_bytes, 8).' - '0';
-    binaryMessage = reshape(binaryMessage, 1, []);
-    
+    messageSymbols = fread(byteFile, 'double');
+    messageSymbols = messageSymbols(messageSymbols == 1 | messageSymbols == -1);
     % Define video parameters
     frameSize = [1440, 1080]; % 1440x1080 / 8 
-    frameRate = 5; 
-    numFrames = length(binaryMessage);
+    frameRate = 25; % Match Ts from encoder
+    numFrames = length(messageSymbols);
     
     % Set up Video Recorder
-    video = VideoWriter('encodedVideo.mp4', 'MPEG-4');
+    video = VideoWriter('encodedVideo', 'MPEG-4');
     video.FrameRate = frameRate;
     open(video)
-    
+
     for i= 1:numFrames
         % Set frame color based on read bit
-        if binaryMessage(i) == 1
+        if messageSymbols(i) > 0
             frame = uint8(ones(frameSize)*255);
         else
             frame = uint8(zeros(frameSize)*255);
